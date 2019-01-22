5f220f4f71b2dbedc6306d98c8f05c99c528e226
HeYuan <heyuan@kokobao.com>
2019-01-22 16:42:40 +0800

    修改导航键启动设置的页面背景

--- app/src/main/java/com/android/ctyon/copyhome/ui/QuickStartActivity.java
+++ app/src/main/java/com/android/ctyon/copyhome/ui/QuickStartActivity.java
@@ -1,7 +1,8 @@
 package com.android.ctyon.copyhome.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
 import com.android.ctyon.copyhome.R;
@@ -11,8 +12,11 @@ public class QuickStartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.perference_set_activity);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_quick_start);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.quick_start_select_title);
         getFragmentManager().beginTransaction().replace(R.id.fragment_layout, new QuickStartFragment()).commit();
     }
 
